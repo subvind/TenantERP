bac451bdb17d2e6cd8c43f3567abdc9129152f3a
istrav <travis.burandt@gmail.com>
2022-05-20 10:22:03 -0500

    leaflet

--- src/routes/geo/index.svelte
+++ src/routes/geo/index.svelte
@@ -1,21 +1,52 @@
+<script context="module" lang="ts">
+  declare module 'leaflet';
+</script>
+
 <script lang="ts">
+  import { onMount } from 'svelte';
+
+  import Map from '$lib/geo/Map.svelte'
+  import Things from '$lib/geo/Things.svelte'
+  import Connections from '$lib/geo/Connections.svelte'
+  import Messages from '$lib/geo/Messages.svelte'
   import About from '$lib/geo/About.svelte'
+
+  onMount(() => {
+    let el = document.querySelectorAll('.tabs')
+    var instance = window['M'].Tabs.init(el, {});
+  })
 </script>
 
 <br />
 <br />
-<div class="wrapper">
-  <div class="title">About</div>
-  <About />
+<div class="container">
+  <Map />
+  <br />
+  <br />
+  <ul class="tabs">
+    <li class="tab col s3"><a class="active" href="#tabs-1">Things</a></li>
+    <li class="tab col s3"><a href="#tabs-2">Connections</a></li>
+    <li class="tab col s3"><a href="#tabs-3">Messages</a></li>
+    <li class="tab col s3"><a href="#tabs-4">About</a></li>
+  </ul>
+  <div id="tabs-1" class="col s12">
+    <Things />
+  </div>
+  <div id="tabs-2" class="col s12">
+    <Connections />
+  </div>
+  <div id="tabs-3" class="col s12">
+    <Messages />
+  </div>
+  <div id="tabs-4" class="col s12">
+    <About />
+  </div>
 </div>
 <br />
 <br />
 
 <style>
-  .wrapper {
-    min-height: 100vh;
-  }
-  .title {
-    text-align: center;
+  .tabs {
+    background: transparent;
   }
 </style>
