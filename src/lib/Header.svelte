1562b4ba89bea9f3ef9dda606f59127fb31d39ef
istrav <travis.burandt@gmail.com>
2022-05-23 01:38:13 -0500

    about

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -8,6 +8,9 @@
 			<a href="//community.istrav.com" class="btn right" style="margin: 1em;">community</a>
       <a href="/" class="brand-logo center">isTrav</a>
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li class:active={$page.url.pathname === '/about'}>
+					<a sveltekit:prefetch href="/about">About</a>
+				</li>
 				<li class:active={$page.url.pathname === '/my-assistant'}>
 					<a sveltekit:prefetch href="/my-assistant">My Assistant</a>
 				</li>
@@ -27,9 +30,6 @@
 				</li>
 				<li class:active={$page.url.pathname === '/projects'}>
 					<a sveltekit:prefetch href="/projects">Projects</a>
-				</li>
-				<li class:active={$page.url.pathname === '/about'}>
-					<a sveltekit:prefetch href="/about">About</a>
 				</li>
 			</ul>
   </nav>
