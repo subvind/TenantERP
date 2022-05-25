3a6913dae2e5a88b2f6b20bc36f48c69250f8b3e
istrav <travis.burandt@gmail.com>
2022-05-25 11:09:05 -0500

    projects loaded from github

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -10,10 +10,16 @@
   }
 
   onMount(async () => {
-    const octokit = new Octokit({ auth: `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` });
-    request = await octokit.rest.repos.listForUser({ username: 'trabur' })
+  let endpoint = `https://projects.trabur.workers.dev`;
+  const init = {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  };
 
-    records = request.data.sort(function (a: any, b: any) {
+  const response = await fetch(endpoint, init);
+  const content: any = JSON.parse(await response.text())
+    records = content.sort(function (a: any, b: any) {
       let bb: any = new Date(b.updated_at)
       let aa: any = new Date(a.updated_at)
       return bb - aa
