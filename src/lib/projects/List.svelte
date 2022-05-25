5442ea99da2c9cbf095704bcb9aacda97e4eec58
istrav <travis.burandt@gmail.com>
2022-05-25 07:49:13 -0500

    projects

---
+++ src/lib/projects/List.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+import { onMount } from "svelte";
+
+  import Time from "svelte-time";
+  export let records: any
+
+  onMount(() => {
+  })
+</script>
+
+<br />
+<br />
+<div class="container">
+  <h1 class="title">
+    Projects
+  </h1>
+  <div class="card">
+    {#each records as record}
+      <div class="card-content">
+        <span class="language">{record.language}</span> 
+        <a href={record.html_url} target="_blank" class="name">{record.name}</a>
+        <br /><span class="description">{record.description}</span>
+        <br /><span class="date"><Time timestamp="{new Date(record.updated_at)}" relative /></span>
+      </div>
+      <hr>
+    {/each}
+    <div class="card-content">
+      <a href="https://github.com/trabur?tab=repositories" class="btn btn-large fluid" target="_blank">all projects</a>
+    </div>
+  </div>
+</div>
+<br />
+<br />
+
+<style>
+	.title {
+		font-weight: 900;
+		font-size: 4.5em;
+		text-align: left;
+		margin: 0;
+	}
+  .card {
+    border: 1em solid #000;
+  }
+  .name {
+    font-size: 1.5em;
+    font-weight: 900;
+  }
+  .language {
+    font-weight: 900;
+    float: right;
+  }
+  .description {
+    font-size: 1.5em;
+  }
+  .card-content {
+    padding: 1em;
+  }
+  .fluid {
+    width: 100%;
+  }
+</style>
--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,20 +1,35 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  import List from '$lib/projects/List.svelte'
   import { Octokit } from 'octokit';
-  // import { Octokit } from "@octokit/rest";
-
-  // https://octokit.github.io/rest.js/v18
   
+  let records: any
+  let request: {
+    data: any
+  }
+
   onMount(async () => {
-    const octokit = new Octokit({ auth: `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` });
-    let data = await octokit.rest.repos.listForUser({ username: 'trabur' })
-    console.log(data)
+    const octokit = new Octokit({ auth: `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` });
+    request = await octokit.rest.repos.listForUser({ username: 'trabur' })
+
+    records = request.data.sort(function (a: any, b: any) {
+      let bb: any = new Date(b.updated_at)
+      let aa: any = new Date(a.updated_at)
+      return bb - aa
+    })
+    console.log(records)
   })
 </script>
-<!-- 
-<br />hello
-<br />hello
-<br />hello
-<br />hello
-<br />hello -->
+
+<div class="wrapper">
+  {#if records}
+    <List records={records} />
+  {/if}
+</div>
+
+<style>
+  .wrapper {
+    min-height: 100vh;
+  }
+</style>
