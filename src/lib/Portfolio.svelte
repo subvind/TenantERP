14f3185d0a5d569baedf3a5643a66450a5d46a97
istrav <travis.burandt@gmail.com>
2022-05-29 02:19:16 -0500

    version

--- src/lib/Portfolio.svelte
+++ src/lib/Portfolio.svelte
@@ -9,7 +9,7 @@
     <h1 class="title">
       Portfolio
     </h1>
-    <marquee direction="right">isTrav, DocBookMD, Staples, Atomic Axis, PNI Media, Gritness, HVAC Buddy, FleetGrid, /community_folder/</marquee>
+    <marquee direction="right">isTrav, DocBookMD, Staples, Atomic Axis, PNI Media, Gritness, HVAC Buddy, FleetGrid, HaloTracks, /community_folder/</marquee>
   </div>
 
 </div>
