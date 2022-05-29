1e76c4cf3309ae21a89fc934b63d65dcaf836b26
istrav <travis.burandt@gmail.com>
2022-05-29 05:01:39 -0500

    about

---
+++ src/routes/about/index.svelte
@@ -0,0 +1,42 @@
+
+
+<br />
+<br />
+<div class="wrapper container">
+  <div class="row">
+    <div class="col m7">
+      <h1 class="title">About</h1>
+      <div class="card box">
+        <div class="card-content">What does Full Stack Developer or FSD mean? Most people would probably tell you that it has something to do with the Front End and Back End of an application. As in the conversation between a client (smart phone, browser) and a server (data center). For me FSD means any source code that needs to be written or maintained; which most modern companies provide programming language level API's to interact with their business. This has added a lot of surface area for FSD to explore and code. There are many videos about this on YouTube. So Full Stack Developer could mean a lot of things. It just depends on what problems you are trying to solve. In this about page I'll be talking about the area that I have explored and the path that I have taken through the land of Full Stack Development. </div>
+      </div>
+    </div>
+    <div class="col m5">
+      <div class=" box">
+        <img src="./travis.jpeg" alt="travis in canada" width="100%" style="padding-bottom: 0.5em; margin-bottom: -1em;" />
+      </div>
+      <div class="card box">
+        <div class="card-content">Travis Burandt is a successful cyclist. At one point he was the youngest and only cat 1 racer in the United States. He has podiumed at states, nationals, and internationally. That was a long time ago though. He has sense then dropped out of racing competatively and now focuses on his own mental health and his own personal coding projects. Lately he has been getting into training an AI bot that will be his <a href="/my-assistant">My Assistant</a>; look out for his profile in the Community for more info.</div>
+      </div>
+    </div>
+  </div>
+</div>
+<br />
+<br />
+
+<style>
+  .wrapper {
+    min-height: 100vh;
+  }
+
+  .box {
+    border: 1em solid #000;
+  }
+  
+	.title {
+		font-weight: 900;
+		font-size: 4.5em;
+		text-align: left;
+		margin: 0;
+    color: #000;
+	}
+</style>
