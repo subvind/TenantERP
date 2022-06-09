f988021203be27cad887eb6ea7845fe5d224e96c
istrav <travis.burandt@gmail.com>
2022-06-09 03:57:09 -0500

    timeline bump

--- src/lib/Ideas.svelte
+++ src/lib/Ideas.svelte
@@ -32,9 +32,11 @@
     <h1 class="title">
       - Platform for ideas.
     </h1>
-    <div class="description">After bouncing ideas back and forth and off the walls product owners should have a solid foundation of like minded thinking built up around what they want and don't want.</div>
-    <br />
-    <a href="/platforms" class="btn btn-large red lighten-2">Platforms</a>
+    <div class="bump">
+      <div class="description">After bouncing ideas back and forth and off the walls product owners should have a solid foundation of like minded thinking built up around what they want and don't want.</div>
+      <br />
+      <a href="/platforms" class="btn btn-large red lighten-2">Platforms</a>
+    </div>
   </div>
 </div>
 <div class="steps">
@@ -42,9 +44,11 @@
     <h1 class="title">
       - Existing ideas.
     </h1>
-    <div class="description">Most ideas are just collections of other ideas. Some ideas are assembled the same yet branded differently. Look here to see how your idea may integrate with other ideas.</div>
-    <br />
-    <a href="/apps" class="btn btn-large red lighten-2">Applications</a>
+    <div class="bump">
+      <div class="description">Most ideas are just collections of other ideas. Some ideas are assembled the same yet branded differently. Look here to see how your idea may integrate with other ideas.</div>
+      <br />
+      <a href="/apps" class="btn btn-large red lighten-2">Applications</a>
+    </div>
   </div>
 </div>
 <div class="steps">
@@ -52,9 +56,11 @@
     <h1 class="title">
       - Fully custom ideas.
     </h1>
-    <div class="description">There are some ideas that may not fit within the scope of our platform or existing applications. Ideas that require a coder to sit down and program. Reach out. We may be able to build your idea and release it as a new feature.</div>
-    <br />
-    <a href="/contact-me" class="btn btn-large red lighten-2">contact me</a>
+    <div class="bump">
+      <div class="description">There are some ideas that may not fit within the scope of our platform or existing applications. Ideas that require a coder to sit down and program. Reach out. We may be able to build your idea and release it as a new feature.</div>
+      <br />
+      <a href="/contact-me" class="btn btn-large red lighten-2">contact me</a>
+    </div>
   </div>
 </div>
 <div class="steps">
@@ -62,9 +68,11 @@
     <h1 class="title">
       - Ideas with solutions.
     </h1>
-    <div class="description">There is a market that every idea should target in order to be successful; for instance, perhaps your niche idea solves a pain point in the "cooking" or (etc) industry.</div>
-    <br />
-    <a href="/solutions" class="btn btn-large red lighten-2">solutions</a>
+    <div class="bump">
+      <div class="description">There is a market that every idea should target in order to be successful; for instance, perhaps your niche idea solves a pain point in the "cooking" or (etc) industry.</div>
+      <br />
+      <a href="/solutions" class="btn btn-large red lighten-2">solutions</a>
+    </div>
   </div>
 </div>
 <div class="steps">
@@ -72,19 +80,23 @@
     <h1 class="title">
       - Successful ideas.
     </h1>
-    <div class="description">Notice how we did a couple of things. We identified the problem. Found a solution. Executed on turning our idea into reality.</div>
-    <br />
-    <a href="/production" class="btn btn-large red lighten-2">production</a>
+    <div class="bump">
+      <div class="description">Notice how we did a couple of things. We identified the problem. Found a solution. Executed on turning our idea into reality.</div>
+      <br />
+      <a href="/production" class="btn btn-large red lighten-2">production</a>
+    </div>
   </div>
 </div>
 <div class="steps">
   <div class="container">
     <h1 class="title">
-      - Idea Management System.
+      - Idea automation.
     </h1>
-    <div class="description">Our client area or Idea Management System (IMS) is where isTrav clients may login to a control panel and keep track of and manage their ideas from startup to production.</div>
-    <br />
-    <a href="/client-area" class="btn btn-large red lighten-2">client area</a>
+    <div class="bump">
+      <div class="description">Our client area or Idea Management Software (IMS) is where isTrav clients may login to a control panel and keep track of and manage their ideas from startup to production.</div>
+      <br />
+      <a href="/client-area" class="btn btn-large red lighten-2">client area</a>
+    </div>
   </div>
 </div>
 <div class="steps">
@@ -92,9 +104,11 @@
     <h1 class="title">
       - Diagnose an idea.
     </h1>
-    <div class="description">Our AI chat bot or "My Assistant" is capable of looking at our historical system logs and database records and can tell you a thing or two about your idea.</div>
-    <br />
-    <a href="/my-assistant" class="btn btn-large red lighten-2">my assistant</a>
+    <div class="bump">
+      <div class="description">Our AI chat bot or "My Assistant" is capable of looking at our historical system logs and database records and can tell you a thing or two about your idea.</div>
+      <br />
+      <a href="/my-assistant" class="btn btn-large red lighten-2">my assistant</a>
+    </div>
   </div>
 </div>
 <div class="container" style="padding: 0;">
@@ -105,6 +119,9 @@
 <h1 class="brand">isTrav</h1>
 
 <style>
+  .bump {
+    padding-left: 3em;
+  }
   .brand {
     text-align: center;
     font-weight: 900;
