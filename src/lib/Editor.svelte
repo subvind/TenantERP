f06eeb59a859205804f8c4dd850ef227192dc319
istrav <travis.burandt@gmail.com>
2022-06-22 10:47:47 -0500

    codemirror

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	
+	import 'codemirror/mode/javascript/javascript'
+	import CodeMirror from './CodeMirror.svelte'
+	import {onMount} from 'svelte'
+	const code = `function test() {\n  return 42\n}`
+	const options = {
+		mode: "javascript",
+		lineNumbers: true,
+		value: code
+	}
+	let editor: any
+	let cursor_activity = false
+	onMount(()=>{
+		console.log("Editor: ", editor)
+	})
+	
+	function cursorMoved(event) {
+		cursor_activity = true
+		console.log('cursor activity')
+		// console.log(event.detail)
+		
+	}
+	
+	function changed(event) {
+		console.log('changed')
+		// console.log(event.detail)
+	}
+	
+
+</script>
+
+<CodeMirror on:activity={cursorMoved} on:change={changed} bind:editor {options} class="editor"/>
+<p>
+	Cursor Activity: {cursor_activity}
+</p>
+<div>
+	<button on:click={() => editor.execCommand('selectAll')}>
+		Select All
+	</button>	
+	
+	<button on:click={() => editor.setCursor(0)}>
+		Cursor at Start
+	</button>	
+
+	<button on:click={() => editor.setCursor(editor.getValue().length)}>
+		Cursor at End
+	</button>	
+
+</div>
+
+
+<style>
+	:global(.editor) {
+		font-size: 1.5rem;
+	}
+</style>
--- src/routes/network-ai/index.svelte
+++ src/routes/network-ai/index.svelte
@@ -1,5 +1,12 @@
 
+<script lang="ts">
+	import Editor from "$lib/Editor.svelte";
+
+</script>
+	
 <svelte:head>
 	<title>Network AI - isTrav</title>
 	<meta name="description" content="Travis Burandt" />
 </svelte:head>
+
+<Editor />
