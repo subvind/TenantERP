4a902f3757c8b08aa321da0f889b2a5a2107323f
istrav <travis.burandt@gmail.com>
2022-09-14 14:47:01 -0500

    resume 3

--- src/routes/resume/index.svelte
+++ src/routes/resume/index.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-
-	function iframeLoaded () {
-		setTimeout(() => {
-			var iFrameID: any = document.getElementById('resume');
-			if (iFrameID) {
-				console.log('iframe loaded', iFrameID.contentWindow.document)
-				// here you can make the height, I delete it first, then I make it again
-				iFrameID.height = "";
-				iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-			}
-		}, 0)
-	}
+	let index: number = 2
+	let links = [
+		{
+			download: 'https://github.com/trabur/2008-2015',
+			website: 'https://2008-2015.istrav.com',
+			height: '2500px',
+			pdf: ''
+		},
+		{
+			download: 'https://github.com/trabur/2016-2021',
+			website: 'https://2016-2021.istrav.com',
+			height: '1000px',
+			pdf: ''
+		},
+		{
+			download: 'https://github.com/trabur/2022-now',
+			website: 'https://2022-now.istrav.com',
+			height: '1000px',
+			pdf: ''
+		},
+	]
 </script>
 
 <svelte:head>
@@ -18,33 +27,31 @@
 	<meta name="description" content="If your looking for talent print out my Resume, send my GitHub to the coding team, and contact me for hire." />
 </svelte:head>
 
-<div class="container">
+<div class="container" style="width: 1100px;">
 	<br />
 	<br />
 	<div style="text-align: center;">
-		<h1>Resume</h1>
-		<a href="#" class="btn large red lighten-2">2008-2015</a>
-		<a href="#" class="btn large red lighten-2">2016-NOW</a>
+		<h1 style="font-weight: 900;">My Resume</h1>
+		<a href="#" on:click={() => index = 0} class={`btn btn-large ${index === 0 ? 'red lighten-2' : 'grey'}`}>2008-2015</a>
+		<a href="#" on:click={() => index = 1} class={`btn btn-large ${index === 1 ? 'red lighten-2' : 'grey'}`}>2016-2021</a>
+		<a href="#" on:click={() => index = 2} class={`btn btn-large ${index === 2 ? 'red lighten-2' : 'grey'}`}>2021-NOW</a>
 	</div>
 	<br />
 	<br />
 	<div style="margin: 0 1em;">
-		<iframe id="resume" src="https://2008-2015.istrav.com" frameborder="0" title="The Resume of Travis Burandt" on:load={() => iframeLoaded()}></iframe>
+		<iframe id="resume" src={links[index].website} height={links[index].height} scrolling="no" frameborder="0" title="The Resume of Travis Burandt"></iframe>
 		<nav class="grey">
 			<div class="nav-wrapper">
 				<div style="text-align: center;">
-					<a href="#" class="btn large black">DOWNLOAD</a>
-					<a href="#" class="btn large black">WEBSITE</a>
-					<a href="#" class="btn large black">PDF</a>
+					<a href={links[index].download} class="btn large black">DOWNLOAD</a>
+					<a href={links[index].website} class="btn large black">WEBSITE</a>
+					<a href={links[index].pdf} class="btn large black">PDF</a>
 				</div>
-	
 			</div>
 		</nav>
 	</div>
 </div>
 
-<br />
-<br />
 <style>
 	h1 {
 		margin: 0;
@@ -54,7 +61,7 @@
 		margin: 0;
 		margin-bottom: -0.4em;
 		min-height: calc(75vh);
-		z-index: 10;
 		border: 1em solid #000;
+		overflow: hidden;
 	}
 </style>
