0e8ee8c08fd98d8acd0bfe4730436cfea02c1b7e
istrav <travis.burandt@gmail.com>
2022-09-18 22:16:09 -0500

    stage zero

--- src/lib/platforms/Rocket.svelte
+++ src/lib/platforms/Rocket.svelte
@@ -1,6 +1,12 @@
 <script>
 </script>
 
+<div class="container">
+  <h1 class="title">
+    Stage Zero
+    <a href="/solutions" class="btn btn-large right red lighten-2" style="margin: 0.5em;">check list</a>
+  </h1>
+</div>
 <div class="white">
   <img src="/graphics/clouds-up.png" alt="" style="width: 100%; position: absolute; top: 0; min-width: 1500px;" />
   <div class="clearfix"></div>
@@ -8,18 +14,30 @@
   <br />
   <br />
   <br />
-  <br />
-  <br />
-  <br />
-  <br />
-  <br />
-  <div style="text-align: center; width: 100%; margin-top: 250px;">
-    <img src="/graphics/rocket.png" alt="" style="width: 150px; margin-left: -75px; position: absolute; bottom: 20px; z-index: 0" /><br />
+  <div class="container">
+    <div style="float: right;">
+      <img src="/graphics/rocket.png" alt="" style="width: 150px; margin-left: -175px; position: absolute; bottom: 20px; z-index: 0" /><br />
+    </div>
+    <div class="row">
+      <div class="col m6">
+        <h4>The most expensive and difficult part to get right is stage zero; because your idea's <span style="text-decoration: underline;">main base</span> of <span style="font-weight: bold;">operations</span> is the most important.</h4>
+      </div>
+    </div>
   </div>
+  <br />
+  <br />
+  <br />
   <img src="/graphics/clouds.png" alt="" style="width: 100%; position: absolute; bottom: 0; min-width: 1500px;" />
 </div>
 
 <style>
+	.title {
+		font-weight: 900;
+		font-size: 4.5em;
+		text-align: left;
+		margin: 0;
+	}
+
   .clearfix::after {
     content: "";
     clear: both;
@@ -33,6 +51,7 @@
   }
 
   .white {
+    margin-top: .5rem;
     padding: 0;
     position: relative;
     display: block;
@@ -41,17 +60,5 @@
     border-top: 1em solid #000;
     border-bottom: 1em solid #000;
     overflow: hidden;
-  }
-
-  .white .header {
-    margin: 0 0 1em;
-    text-align: center;
-  }
-
-  .chart {
-    background: #eee;
-    border-radius: 2em;
-    min-height: 200px;
-    padding: 1.5em;
   }
 </style>
