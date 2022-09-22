b6d7b56c6c8cb6eec13f2dcadf57657af9455f88
istrav <travis.burandt@gmail.com>
2022-09-22 16:35:39 -0500

    Cultivate Ideas

--- src/lib/innovation/Header.svelte
+++ src/lib/innovation/Header.svelte
@@ -1,11 +1,22 @@
 
-<div class="fair-source">
+<div class="cultivate">
   <div class="container">
     <h1 class="title">
-      From: Idea<br />To: Production
+      Cultivate Ideas
     </h1>
     <!-- svelte-ignore a11y-distracting-elements -->
-    <div class="description">or applications are pieces of software designed and written to fulfill a particular purpose of the user. The primary reason why people choose our software is because they believe in our principals. Others choose us because of all the innovation that we have done to our features and our solutions solve their pain points for their unique industry(s).</div>
+    <div class="description">With a practical Innovation Management System (IMS):</div>
+    <ul class="pattern">
+      <li>
+        users may: implement ideas that result in the introduction of new goods or services or improvement in offering goods or services; it is for <span style="text-decoration: underline;">innovation</span>.
+      </li>
+      <li>
+        users may: track the progress of ideas and make decisions for further development; it is for <span style="font-weight: 900;">observability</span>.
+      </li>
+      <li>
+        users may: keep a set of policies, processes, and procedures (as an organization) to ensure that ideas fulfill the tasks required to achieve their objectives; it is a <span style="text-decoration: underline;">management system</span>.
+      </li>
+    </ul>
     <br />
     <a href="/innovation" class="btn btn-large white black-text">Innovation Management System</a>
   </div>
@@ -15,20 +26,27 @@
   .container {
     padding: 5em 0;
   }
-  .fair-source {
+  .cultivate {
     border-bottom: 1em solid #000;
     background-color: #222222;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='152' height='152' viewBox='0 0 152 152'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='temple' fill='%23333333' fill-opacity='1'%3E%3Cpath d='M152 150v2H0v-2h28v-8H8v-20H0v-2h8V80h42v20h20v42H30v8h90v-8H80v-42h20V80h42v40h8V30h-8v40h-42V50H80V8h40V0h2v8h20v20h8V0h2v150zm-2 0v-28h-8v20h-20v8h28zM82 30v18h18V30H82zm20 18h20v20h18V30h-20V10H82v18h20v20zm0 2v18h18V50h-18zm20-22h18V10h-18v18zm-54 92v-18H50v18h18zm-20-18H28V82H10v38h20v20h38v-18H48v-20zm0-2V82H30v18h18zm-20 22H10v18h18v-18zm54 0v18h38v-20h20V82h-18v20h-20v20H82zm18-20H82v18h18v-18zm2-2h18V82h-18v18zm20 40v-18h18v18h-18zM30 0h-2v8H8v20H0v2h8v40h42V50h20V8H30V0zm20 48h18V30H50v18zm18-20H48v20H28v20H10V30h20V10h38v18zM30 50h18v18H30V50zm-2-40H10v18h18V10z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 	}
-  .fair-source .title {
+  .cultivate .title {
     font-weight: 900;
     font-size: 4.5em;
     text-align: left;
     margin: 0;
     color: #fff;
   }
-  .fair-source .description {
+  .cultivate .description {
     color: #fff;
     font-size: 1.5em;
+  }
+  .cultivate .pattern {
+    color: #ccc;
+    font-size: 1.5em;
+  }
+  .cultivate .pattern li {
+    list-style-type: initial;
   }
 </style>
