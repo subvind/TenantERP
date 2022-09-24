c272adbbef7fb16d605c1e976e2328800e37d36e
istrav <travis.burandt@gmail.com>
2022-09-23 19:52:25 -0500

    enabled

--- src/lib/production/Footer.svelte
+++ src/lib/production/Footer.svelte
@@ -4,7 +4,7 @@
 <br />
 <br />
 <div class="container">
-  <h1 class="title black-text">Enable Ideas</h1>
+  <h1 class="title black-text">Enabled Ideas</h1>
   <div class="description black-text">Invitation to join an Innovation Management System (IMS)...</div>
   <br />
   <div class="description black-text">What makes an idea ready? Perhaps a few things. Does it have critical infrastructure or software that must be deployed and updated regularly? Does it have a single repository where the source code to that software may be looked at? Does it have tests? Does it have a status page? Does it have a deployment pipeline? Does it have a trell board with swim lanes for pending, in development, testing, released, etc? When it goes down is someone alerted?</div>
