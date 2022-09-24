a56fa91e68b8dbf6551d3511761c418fd990bbf2
istrav <travis.burandt@gmail.com>
2022-09-23 19:34:35 -0500

    spacing

--- src/lib/client-area/Marketing.svelte
+++ src/lib/client-area/Marketing.svelte
@@ -10,11 +10,12 @@
   <br />
   <div class="description">Feeling stuck? Take a look at our headquarters which could answer any questions that you may still have lingering around.</div>
   <br />
+  <br />
   <a href="https://istrav.dev" class="btn btn-large white black-text"><i class="material-icons left">developer_board</i> system headquarters</a>
   <br />
   <br />
   <br />
-  <h2 class="title" style="font-size: 3em; line-height: 1.5em;">
+  <h2 class="title" style="font-size: 3em; line-height: 1.5em; position: absolute; bottom: 1em;">
     <span style="border-bottom: 0.2em solid #ee6e73;">Accelerate</span> <span style="border-bottom: 0.2em solid #ee6e73;">progress</span>
     <br /><span style="border-bottom: 0.2em solid #aaa;">and</span> <span style="border-bottom: 0.2em solid #64B5F6;">change</span> <span style="border-bottom: 0.2em solid #64B5F6;">quickly</span>
     <br /><span style="border-bottom: 0.2em solid #aaa;">with</span> <span style="border-bottom: 0.2em solid #26a69a;">isTrav</span>.
--- src/lib/pricing/Header.svelte
+++ src/lib/pricing/Header.svelte
@@ -9,6 +9,7 @@
     <br />
     <div class="description">Free forever up until you reach the Fair Source 25 (v0.9). license agreement limit. Checkout our Pricing Level slider below to calculate usage limits (active users, requests per day, requests per mounth) with expected costs per month.</div>
     <br />
+    <br />
     <a href="/about">
       <img src="/anchor.png" alt="" class="anchor">
     </a>
