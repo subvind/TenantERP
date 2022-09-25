87d129a4ba66460338fc869217a3b858a0d4aa52
istrav <travis.burandt@gmail.com>
2022-09-25 14:48:57 -0500

    market

--- src/lib/innovation/Footer.svelte
+++ src/lib/innovation/Footer.svelte
@@ -7,7 +7,7 @@
   <h1 class="title black-text">Great Ideas</h1>
   <div class="description black-text">Invitation to join an Innovation Management System (IMS)...</div>
   <br />
-  <div class="description black-text">What makes an idea above average? Perhaps a few things. Does it have a name that others can easily hear, say, and identify? Does it exist in a place that makes it special? Does it have a team behind it that is motivated? Does it have a design so good that others know it by word of mouth. Does it open up a new maket for learning or business? When it comes to the test of time will others still be using it?</div>
+  <div class="description black-text">What makes an idea above average? Perhaps a few things. Does it have a name that others can easily hear, say, and identify? Does it exist in a place that makes it special? Does it have a team behind it that is motivated? Does it have a design so good that others know it by word of mouth. Does it open up a new market for industries? When it comes to the test of time will others still be using it?</div>
   <br />
   <br />
   <a href="/pricing" class="btn btn-large black">pricing levels</a>
