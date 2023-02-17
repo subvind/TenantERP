34d036ad15ea8373862d1b4d5740a1736d880fde
istrav <travis.burandt@gmail.com>
2023-02-17 17:27:23 -0600

    align

--- src/lib/Branding.svelte
+++ src/lib/Branding.svelte
@@ -18,7 +18,7 @@
               <div class="names">I am a hard worker. My favorite thing to see is how tech disrupts society. Especially in films like Star Trek.</div> 
               <br />
               <div>ABOUT:</div>
-              <div class="names">[Manifesto, Strategy, Vision, Values, Status, Contribute, Target]</div> 
+              <div class="names">[Manifesto, Strategy, Tactics, Vision, Values, Status, Contribute, Target]</div> 
               <div></div>   
             </div>
           </div>
