07310e31abfcc86ba511dfc92eae22095db95c6a
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-09-05 18:03:11 -0500

    homepage lingo

--- src/lib/Ideas.svelte
+++ src/lib/Ideas.svelte
@@ -37,84 +37,120 @@
 <div class="steps">
   <div class="container">
     <h3 class="title">
-      - Platform for ideas.
+      - ERP Software
     </h3>
     <div class="bump">
-      <div class="description">After bouncing ideas back and forth and off the walls product owners should have a solid foundation of like minded thinking built up around what they want and don't want.</div>
-      <br />
-      <a href="/platforms" class="btn btn-large red lighten-2"><i class="material-icons left">folder</i>Platforms</a>
+      <div class="description">ERP software is a comprehensive business management solution that integrates various functions like finance, human resources, inventory, and manufacturing into a single system. It enables real-time data access and facilitates efficient decision-making.</div>
+      <!-- <br /> -->
+      <!-- <a href="/platforms" class="btn btn-large red lighten-2"><i class="material-icons left">folder</i>Platforms</a> -->
     </div>
   </div>
 </div>
 <div class="steps">
   <div class="container">
     <h3 class="title">
-      - Existing ideas.
+      - ERP Modules
     </h3>
     <div class="bump">
-      <div class="description">Most ideas are just collections of other ideas. Some ideas are assembled the same yet branded differently. Look here to see how your idea may integrate with other ideas.</div>
-      <br />
-      <a href="/apps" class="btn btn-large red lighten-2"><i class="material-icons left">extension</i>Applications</a>
+      <div class="description">ERP systems consist of various modules, each addressing specific business processes. These modules can include finance, HR, supply chain management, CRM, and more, allowing companies to tailor their ERP to their unique needs.</div>
+      <!-- <br /> -->
+      <!-- <a href="/apps" class="btn btn-large red lighten-2"><i class="material-icons left">extension</i>Applications</a> -->
     </div>
   </div>
 </div>
 <div class="steps">
   <div class="container">
     <h3 class="title">
-      - Fully custom ideas.
+      - ERP Implementation
     </h3>
     <div class="bump">
-      <div class="description">There are some ideas that may not fit within the scope of our platform or existing applications. Ideas that require a coder to sit down and program. Reach out. We may be able to build your idea and release it as a new feature.</div>
-      <br />
-      <a href="/contact-me" class="btn btn-large red lighten-2"><i class="material-icons left">face</i>contact me</a>
+      <div class="description">ERP implementation is the process of deploying and configuring the ERP software to meet a company's requirements. It involves data migration, training, and customization to ensure a smooth transition to the new system.</div>
+      <!-- <br /> -->
+      <!-- <a href="/contact-me" class="btn btn-large red lighten-2"><i class="material-icons left">face</i>contact me</a> -->
     </div>
   </div>
 </div>
 <div class="steps">
   <div class="container">
     <h3 class="title">
-      - Ideas with solutions.
+      - Cloud ERP
     </h3>
     <div class="bump">
-      <div class="description">There is a market that every idea should target in order to be successful; for instance, perhaps your niche idea solves a pain point in the "cooking" or (etc) industry.</div>
-      <br />
-      <a href="/innovation/solutions" class="btn btn-large red lighten-2"><i class="material-icons left">playlist_add_check</i>solutions</a>
+      <div class="description">Cloud ERP refers to ERP systems hosted in the cloud. It offers the benefits of scalability, accessibility from anywhere, and reduced IT infrastructure costs. Cloud ERP solutions are increasingly popular for their flexibility.</div>
+      <!-- <br /> -->
+      <!-- <a href="/innovation/solutions" class="btn btn-large red lighten-2"><i class="material-icons left">playlist_add_check</i>solutions</a> -->
     </div>
   </div>
 </div>
 <div class="steps">
   <div class="container">
     <h3 class="title">
-      - Successful ideas.
+      - ERP Integration
     </h3>
     <div class="bump">
-      <div class="description">Notice how we did a couple of things. We identified the problem. Found a solution. Executed on turning our idea into reality.</div>
-      <br />
-      <a href="/production" class="btn btn-large red lighten-2"><i class="material-icons left">check</i>production</a>
+      <div class="description">ERP integration involves connecting the ERP system with other software applications used within an organization. This integration ensures seamless data flow and improved business processes across departments.</div>
+      <!-- <br /> -->
+      <!-- <a href="/production" class="btn btn-large red lighten-2"><i class="material-icons left">check</i>production</a> -->
     </div>
   </div>
 </div>
 <div class="steps">
   <div class="container">
     <h3 class="title">
-      - Idea automation.
+      - ERP Vendor Selection
     </h3>
     <div class="bump">
-      <div class="description">Our client area or Idea Management Software (IMS) is where isTrav clients may login to a control panel and keep track of and manage their ideas from startup to production.</div>
-      <br />
-      <a href="/client-area" class="btn btn-large red lighten-2"><i class="material-icons left">security</i>client area</a>
+      <div class="description">Choosing the right ERP vendor is crucial for a successful implementation. This process involves evaluating vendors based on factors like system features, cost, scalability, and the vendor's track record.</div>
+      <!-- <br /> -->
+      <!-- <a href="/client-area" class="btn btn-large red lighten-2"><i class="material-icons left">security</i>client area</a> -->
     </div>
   </div>
 </div>
 <div class="steps">
   <div class="container">
     <h3 class="title">
-      - Diagnose an idea.
+      - ERP Benefits
     </h3>
     <div class="bump">
-      <div class="description">Our team of AI chatbots are capable of looking at our historical system logs and database records and can tell you a thing or two about your idea.</div>
-      <br />
-      <a href="https://istrav.info" class="btn btn-large red lighten-2"><i class="material-icons left">chat</i>Information</a>
+      <div class="description">ERP systems offer numerous benefits, such as improved efficiency, reduced operational costs, better decision-making through data analytics, and enhanced customer service, ultimately leading to increased profitability.</div>
+      <!-- <br /> -->
+      <!-- <a href="https://istrav.info" class="btn btn-large red lighten-2"><i class="material-icons left">chat</i>Information</a> -->
+    </div>
+  </div>
+</div>
+<div class="steps">
+  <div class="container">
+    <h3 class="title">
+      - ERP Security
+    </h3>
+    <div class="bump">
+      <div class="description">ERP security focuses on safeguarding sensitive data and preventing unauthorized access. It includes measures like user authentication, data encryption, and regular security audits to protect valuable business information.</div>
+      <!-- <br /> -->
+      <!-- <a href="https://istrav.info" class="btn btn-large red lighten-2"><i class="material-icons left">chat</i>Information</a> -->
+    </div>
+  </div>
+</div>
+<div class="steps">
+  <div class="container">
+    <h3 class="title">
+      - ERP Mobile Applications
+    </h3>
+    <div class="bump">
+      <div class="description">ERP mobile applications allow users to access critical business data and functions on their mobile devices. This mobility enhances productivity by enabling employees to make informed decisions on the go.</div>
+      <!-- <br /> -->
+      <!-- <a href="https://istrav.info" class="btn btn-large red lighten-2"><i class="material-icons left">chat</i>Information</a> -->
+    </div>
+  </div>
+</div>
+<div class="steps">
+  <div class="container">
+    <h3 class="title">
+      - ERP for Small Bunisnesses
+    </h3>
+    <div class="bump">
+      <div class="description">ERP systems are not exclusive to large enterprises; they are also available for small and medium-sized businesses. These scaled-down ERP solutions help smaller companies manage their operations efficiently and compete in the market.</div>
+      <!-- <br /> -->
+      <!-- <a href="https://istrav.info" class="btn btn-large red lighten-2"><i class="material-icons left">chat</i>Information</a> -->
     </div>
   </div>
 </div>
--- src/lib/ReadMe.svelte
+++ src/lib/ReadMe.svelte
@@ -12,10 +12,7 @@
               Hello There!
               <br />My name is Travis. I am a full stack developer. This collection of websites and apps that are all under the anchor logo and isTrav branding are for research, learning, and exploring purposes. Take a look at our <a href="/about">istrav.com/about</a> page for more point-of-reference and philosphy.
             </span>
-            <br /><br />This website, <a href="//istrav.com">isTrav</a> runs 100% on the <span style="text-decoration: underline;">client</span> and will deploy to any architecture. It is built from scratch on a shoe string buget. So it has amazing uptime, expensive, and less likely to have code rot.
-            <br /><br />My platform, <a href="//istrav.net" target="_blank">/istrav/</a> runs 100% on the <span style="text-decoration: underline;">server</span> and will deploy to any container serivce. It is fully customizable and white-labeled so others may fork it. It has a Fair Source 25 (v0.9) license so it's free up until the 25 active users limit is reached.
-            <br /><br />Your platform, <a href="#!">/brandx/</a> runs just like my platform except you have complete access to run it as you wish for as long as you follow the same license agreement.
-            <br /><br />The <a href="/client-area">istrav.com/client-area</a> is where members are granted access control panels. See <a href="/pricing">istrav.com/pricing</a> for advertising limits and no hidden fees. Thank you for your support!
+            <br /><br />The <a href="/client-area">istrav.com/client-area</a> is where members are granted access control panels. See <a href="/pricing">istrav.com/pricing</a> for branding removal and license fees. Thank you for your support!
           </p>
         </div>
         <div class="card-action">
