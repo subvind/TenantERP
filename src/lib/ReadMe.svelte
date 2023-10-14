e4bfbeffbf77c84916df7ba9cdcd2577551fb998
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-10-13 19:34:00 -0500

    hello there

--- src/lib/ReadMe.svelte
+++ src/lib/ReadMe.svelte
@@ -5,15 +5,28 @@
         <img src="travis.jpeg" class="image" alt="Travis Burandt">
         <div class="card-content">
           <div class="card-title">
-            README.md
+            Hello There!
           </div>
-          <p>
-            <span style="font-weight: bold;">
-              Hello There!
-              <br />My name is Travis. I am a full stack developer. This collection of websites and apps that are all under the anchor logo and isTrav branding are for research, learning, and exploring purposes. Take a look at our <a href="/about">istrav.com/about</a> page for more point-of-reference and philosphy.
-            </span>
-            <br /><br />The <a href="https://client-area.istrav.com">client-area.istrav.com</a> is where members are granted access control panels. See <a href="/pricing">istrav.com/pricing</a> for branding removal and license fees. Thank you for your support!
-          </p>
+          Thank you for taking the time in reviewing us for your consideration.
+          <br />
+          <br />
+          Websites are like sail boats. They require a main haul or backend API. They require a main mast or frontend interface. When put together and under the right wind-conditions/user-interations your boat/project can successfully navigate to it's destination.
+          <br />
+          <blockquote cite="">
+            <a href="https://underwind.solutions">https://underwind.solutions</a>
+          </blockquote>
+          Say hello to underwind.solutions: where your project is not just another website; because we treat your project as if it were a high end yacht. We're not as expensive as you might think though, as we only charge per request. So no wind-under-you-sails/user-interactions then no movement/charge.
+          <br />
+          <br />
+          Therefore, the better user experience we create, the more user-interactions your website will get, thus the more profit we can both make. 
+          <br />
+          <br />
+          A wind wind situation :)
+          <br />
+          <br />
+          kind regards,
+          <br />
+          underwind // subvind
         </div>
         <div class="card-action">
           <p>
